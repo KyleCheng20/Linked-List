f98dacc7977db739139f94f4cc7a5bdcc899f2d9
Kyle Cheng <kcheng120@gmail.com>
2026-02-18 15:29:42 -0500

    Complete append(value) method

---
+++ linkedList.js
@@ -0,0 +1,31 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+    }
+
+    append(value){
+        const newNode = new Node(value);
+
+        // If list is empty and first node in list
+        if(this.head === null){
+            this.head = newNode;
+            return;
+        }
+
+        // If list is not empty
+        let current = this.head;
+        while(current.next !== null){
+            current = current.next;
+        }
+        // Update previous tail node to point to new node
+        current.next = newNode;
+    }
+}
+
